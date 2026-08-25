5f1008056f667e03708e266a00afcf363484160d
pleshw <pleshw@outlook.com>
2026-08-25 16:08:26 -0300

    atualiza mapeamento de CreateUserResult para incluir o nome do usuário e remove comentários desnecessários

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Users/CreateUser/CreateUserResult.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Users/CreateUser/CreateUserResult.cs
@@ -1,12 +1,10 @@
-﻿namespace Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Application.Users.CreateUser;
 
 /// <summary>
 /// Represents the response returned after successfully creating a new user.
 /// </summary>
-/// <remarks>
-/// This response contains the unique identifier of the newly created user,
-/// which can be used for subsequent operations or reference.
-/// </remarks>
 public class CreateUserResult
 {
     /// <summary>
@@ -14,4 +12,14 @@ public class CreateUserResult
     /// </summary>
     /// <value>A GUID that uniquely identifies the created user in the system.</value>
     public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; }
+
+    public UserStatus Status { get; set; }
 }
